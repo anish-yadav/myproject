98116db9aaef0d771a494f87ebd21238c63289db
Anish Kumar Yadav <40102074+anish-yadav@users.noreply.github.com>
2020-10-01 08:53:28 +0530

    Update multiplesinbetween1000of3or5.java
    Added formatting

--- multiplesinbetween1000of3or5.java
+++ multiplesinbetween1000of3or5.java
@@ -2,17 +2,17 @@ public class multiplesinbetween1000of3or5
 {
 	public static void main(String[]args)
 	{
-		   int sum=0;
-		 System.out.println("All multiples of 3 or 5::");
-	   for(int i=1;i<1000;i++)
+		 int sum=0;
+		 System.out.println("All multiples of 3 or 5 ::");
+	   for(int i=3;i<1000;i++)
 	   {	
 		 if(i%3==0 || i%5==0)
 		 {
-			System.out.println("numbers are::"+i);
+			System.out.println("numbers are ::"+i);
 			sum=sum+i; 
 		 }
 	   }
-		 System.out.println("Sum of all multiples of 3or 5::"+sum);
+	   System.out.println("Sum of all multiples of 3 or 5::"+sum);
 	 }
 	
 }
